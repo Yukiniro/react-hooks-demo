c7c0cf7e3680f3ec2431d6b38ab1d93272f9b5b8
Yukiniro <yukiniro@hotmail.com>
2022-11-27 21:48:10 +0800

    feat: add useState playground

---
+++ playground/useState/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const increase = () => setCount(count + 1);
+  return (
+    <div className="App">
+      <h1>useState</h1>
+      <p>Count is {count}</p>
+      <button onClick={increase}>Click me to increase count</button>
+    </div>
+  );
+}
+
+export default App;
