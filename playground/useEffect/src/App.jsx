d1b6fc11dee3e5f7f499ad00c542f9dc6a8a89be
Yukiniro <yukiniro@hotmail.com>
2022-12-05 07:58:29 +0800

    feate: daily update

--- playground/useEffect/src/App.jsx
+++ playground/useEffect/src/App.jsx
@@ -1,19 +1,38 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const increase = () => setCount(count => count + 1);
+  const [color, setColor] = useState("#ed5736"); // #ED5736 rgb(127,236,173)
+  const titleRef = useRef(null);
+  const handleClick = () =>
+    setTimeout(() => {
+      setColor(color => (color === "#ed5736" ? "#7fecad" : "#ed5736"));
+    });
+
+  window.testClick = handleClick;
+
+  // const handleClick = () =>
+  //   setColor(color => (color === "#ed5736" ? "#7fecad" : "#ed5736"));
 
   useEffect(() => {
-    console.log(`Render Count: ${count}`);
-  }, [count]);
+    // console.time();
+    // let i = 0;
+    // while (i < 1000000000) {
+    //   i++;
+    // }
+    // console.timeEnd();
+    titleRef.current.style.background = "#ed5736";
+  });
+
+  const style = { background: color };
 
   return (
     <div className="App">
-      <h1>useEffect</h1>
-      <p>Open chrome devtools to check the log info.</p>
-      <button onClick={increase}>Click me to increase count</button>
+      <h1 style={style} ref={titleRef}>
+        useEffect
+      </h1>
+      <p style={style}>Current color is: {color}</p>
+      <button onClick={handleClick}>Change Color</button>
     </div>
   );
 }
--- playground/useLayoutEffect/src/App.jsx
+++ playground/useLayoutEffect/src/App.jsx
@@ -1,19 +1,32 @@
-import { useState, useEffect } from "react";
+import { useState, useLayoutEffect, useRef } from "react";
 import "./App.css";
+import rgbToHex from "rgb-hex";
+import { comelog } from "comelog";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const increase = () => setCount(count => count + 1);
+  const [color, setColor] = useState("#ed5736"); // #ED5736 rgb(127,236,173)
+  const titleRef = useRef(null);
+  const handleClick = () =>
+    setColor(color => (color === "#ed5736" ? "#7fecad" : "#ed5736"));
 
-  useEffect(() => {
-    console.log(`Render Count: ${count}`);
-  }, [count]);
+  useLayoutEffect(() => {
+    const bkColor = `#${rgbToHex(titleRef.current.style.background)}`;
+    if (bkColor === color) {
+      comelog.info(bkColor);
+    } else {
+      comelog.error(bkColor);
+    }
+  });
+
+  const style = { background: color };
 
   return (
     <div className="App">
-      <h1>useEffect</h1>
-      <p>Open chrome devtools to check the log info.</p>
-      <button onClick={increase}>Click me to increase count</button>
+      <h1 style={style} ref={titleRef}>
+        useLayoutEffect
+      </h1>
+      <p style={style}>Current color is: {color}</p>
+      <button onClick={handleClick}>Change Color</button>
     </div>
   );
 }
