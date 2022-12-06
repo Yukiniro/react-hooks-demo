5f235144cb9fbcf0c076de9e9d75d4502a928ca7
Yukiniro <yukiniro@hotmail.com>
2022-12-06 22:12:51 +0800

    feat: done useMemo && useCallback

--- playground/useMemoAndCallback/src/App.jsx
+++ playground/useMemoAndCallback/src/App.jsx
@@ -1,5 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState, useMemo, useEffect, useCallback } from "react";
 import "./App.css";
+import { comelog } from "comelog";
 
 function longCalcEffect() {
   let i = 0;
@@ -9,34 +10,39 @@ function longCalcEffect() {
 }
 
 function getBigNumber(value) {
-  // longCalcEffect();
-  // longCalcEffect();
+  longCalcEffect();
+  longCalcEffect();
   return value * 10;
 }
 
-let renderCount = 0;
-
 function App() {
   const [, setBaseNumber] = useState(1);
-  const handleClick = () => {
+
+  // const handleClick = useCallback(() => {
+  //   setBaseNumber(parseInt(Math.random(0, 1) * 100));
+  // }, []);
+
+  const handleClick = useCallback(() => {
     setBaseNumber(parseInt(Math.random(0, 1) * 100));
-  };
+  }, []);
 
-  useEffect(() => {
-    console.log("render");
-  });
-
-  renderCount++;
   const startTime = Date.now();
-  const number = getBigNumber(1);
+  const number = useMemo(() => getBigNumber(1), []);
   const endTime = Date.now();
   const usedTime = endTime - startTime;
+
+  useEffect(() => {
+    comelog.info("Render");
+  });
+
+  useEffect(() => {
+    comelog.warn("Render only mount or handleClick changed");
+  }, [handleClick]);
 
   return (
     <div className="App">
       <h1>useMemo && useCallback</h1>
       <button onClick={handleClick}>Refresh</button>
-      <p>RenderCount is: {renderCount}</p>
       <p>BigNumber is: {number}</p>
       <p>Used Time is: {usedTime}ms</p>
     </div>
